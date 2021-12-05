ed53b0b1f7b5247bb19c16c22d57a79947d1538a
qilei123 <qileimail123@gmail.com>
2021-12-05 17:32:40 -0500

    test

---
+++ train_huimangbas_3cls.sh
@@ -0,0 +1,1 @@
+python train.py /data/qilei/.DATASETS/diabetic-retinopathy-detection --model convit_base --pretrained --num-classes 3 --output /data/qilei/.DATASETS/diabetic-retinopathy-detection/work_dir -b 64
