00956abfcce0f54abca749b922dea25016711aec
qilei123 <qileimail123@gmail.com>
2021-12-06 15:48:02 -0500

    test

--- validation_GPC.sh
+++ validation_GPC.sh
@@ -4,12 +4,18 @@ export num_classes=12
 export output_dir=${data_dir}work_dir/
 export batch_size=16
 
-#model_name=vit_base_patch32_224
-#python train.py $data_dir --model $model_name --pretrained --num-classes $num_classes --output $output_dir -b $batch_size --use_balanced_sampler
-#model_name=twins_pcpvt_base
-#python train.py $data_dir --model $model_name --pretrained --num-classes $num_classes --output $output_dir -b $batch_size --use_balanced_sampler
+model_name=vit_base_patch32_224
+checkpoint_dir=$output_dir/$model_name-224/model_best.pth.tar
+python validate.py $data_dir --model $model_name --num-classes $num_classes --checkpoint $checkpoint_dir -b $batch_size --confusion-matrix --confusion-matrix-fig-dir $output_dir/$model_name-224
+
+model_name=twins_pcpvt_base
+checkpoint_dir=$output_dir/$model_name-224/model_best.pth.tar
+python validate.py $data_dir --model $model_name --num-classes $num_classes --checkpoint $checkpoint_dir -b $batch_size --confusion-matrix --confusion-matrix-fig-dir $output_dir/$model_name-224
+
 model_name=xcit_small_12_p8_224
 checkpoint_dir=$output_dir/$model_name-224/model_best.pth.tar
 python validate.py $data_dir --model $model_name --num-classes $num_classes --checkpoint $checkpoint_dir -b $batch_size --confusion-matrix --confusion-matrix-fig-dir $output_dir/$model_name-224
-#model_name=swin_base_patch4_window7_224
-#python train.py $data_dir --model $model_name --pretrained --num-classes $num_classes --output $output_dir -b $batch_size --use_balanced_sampler
+
+model_name=swin_base_patch4_window7_224
+checkpoint_dir=$output_dir/$model_name-224/model_best.pth.tar
+python validate.py $data_dir --model $model_name --num-classes $num_classes --checkpoint $checkpoint_dir -b $batch_size --confusion-matrix --confusion-matrix-fig-dir $output_dir/$model_name-224
