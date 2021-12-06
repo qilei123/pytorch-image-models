735b5c4dc7e2f16d6e3abe39aecbe3eb81e60164
qilei123 <qileimail123@gmail.com>
2021-12-06 15:00:00 -0500

    test

--- show_tools.py
+++ show_tools.py
@@ -93,4 +93,4 @@ def plot_confusion_matrix(cm,
     plt.tight_layout()
     plt.ylabel('True label')
     plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
-    plt.savefig('plot_folder/%s.jpg' %(fig_name))
+    plt.savefig('%s.jpg' %(fig_name))
