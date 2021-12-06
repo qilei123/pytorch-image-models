53549c47ce07cdd07db1d5c70364aa64926d49b9
qilei123 <qileimail123@gmail.com>
2021-12-06 11:30:27 -0500

    test

--- train_GPC.sh
+++ train_GPC.sh
@@ -1,7 +1,7 @@
 export data_dir=/home/qilei/.TEMP/gastro_position_clasification/
 export num_classes=12
 export output_dir=${data_dir}work_dir_balanced/
-export batch_size=64
+export batch_size=16
 
 #model_name=vit_base_patch32_224
 #python train.py $data_dir --model $model_name --pretrained --num-classes $num_classes --output $output_dir -b $batch_size --use_balanced_sampler
