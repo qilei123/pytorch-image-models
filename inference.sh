4be60d1171c141635507213e4d67a1161c6ea5c7
qilei123 <qileimail123@gmail.com>
2021-12-21 18:13:21 -0500

    test

--- inference.sh
+++ inference.sh
@@ -1,5 +1,5 @@
 python inference.py /home/qilei/.TEMP/gastro_position_clasification_11/test/ \
-    --output_dir /home/qilei/.TEMP/gastro_position_clasification_11/result/ \
+    --output_dir /home/qilei/.TEMP/gastro_position_clasification_11/work_dir/vit_base_patch32_224-224/ \
     -m vit_base_patch32_224 \
     --num-classes 12 \
     -b 32 \
