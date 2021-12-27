82ffa44359fe61043bac9dc2e1b10a7da5e31462
qilei123 <qileimail123@gmail.com>
2021-12-27 11:22:27 -0500

    test

--- test_inference.py
+++ test_inference.py
@@ -24,7 +24,7 @@ _logger = logging.getLogger('inference')
 def test_inference():
 
     checkpoint_path = '/home/qilei/.TEMP/gastro_position_clasification_11/work_dir/swin_base_patch4_window7_224-224/model_best.pth.tar'
-    file_path = ''
+    file_path = '/home/qilei/.TEMP/gastro_position_clasification_11/test/0/20191015_1601_1610_w_779.jpg'
 
     model = create_model(
         'swin_base_patch4_window7_224',
