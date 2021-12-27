e79e65f3748e7d810d57a6ba7ea40d6e3092ef05
qilei123 <qileimail123@gmail.com>
2021-12-27 14:31:31 -0500

    test

--- generate_pca.py
+++ generate_pca.py
@@ -1,5 +1,6 @@
 import test_inference
 from test_inference import *
+from timm.data import ImageDataset, create_loader
 
 def get_single_image_feature(image,model,transform):
     if isinstance(image,str):
@@ -20,16 +21,40 @@ def get_single_image_feature(image,model,transform):
 
 def generate_pca():
     checkpoint_path = '/home/qilei/.TEMP/gastro_position_clasification_11/work_dir/swin_base_patch4_window7_224-224/clean_model_best.pth'
-    img_path = '/home/qilei/.TEMP/gastro_position_clasification_11/test/0/20191015_1601_1610_w_779.jpg'
+    img_path = '/home/qilei/.TEMP/gastro_position_clasification_11/test/'
     model_name = 'swin_base_patch4_window7_224'
     
-    model, transform = init_model(model_name,checkpoint_path)
+    model = create_model(
+        model_name,
+        num_classes=12,
+        in_chans=3,
+        pretrained=False,
+        checkpoint_path=checkpoint_path)
 
-    image = cv2.imread(img_path)
+    config = resolve_data_config({}, model=model)
+    transform = create_transform(**config)
 
-    out_feature = get_single_image_feature(image,model,transform)
+    model = model.cuda()
+    model.eval()
 
-    print(out_feature)
+    loader = create_loader(
+        ImageDataset(img_path),
+        input_size=config['input_size'],
+        batch_size=32,
+        use_prefetcher=True,
+        interpolation=config['interpolation'],
+        mean=config['mean'],
+        std=config['std'],
+        num_workers=2,
+        crop_pct=1.0)
+    out_features = torch.zeros((0, 1024), dtype=torch.float32)
+    with torch.no_grad():
+        for batch_idx, (input, _) in enumerate(loader):
+            input = input.cuda()
+            out_feature = model.forward_features(input)
+            out_features = torch.cat((out_features, out_feature.detach().cpu()), 0)
+
+    print(out_features.shape)
 
 
 if __name__ == '__main__':
