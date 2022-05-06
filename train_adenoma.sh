b349ee03fc86dbc5bac775c5edf01ea871e25130
qilei123 <qileimail123@gmail.com>
2022-05-06 18:20:51 -0400

    test

--- train_adenoma.sh
+++ train_adenoma.sh
@@ -11,6 +11,7 @@ num_class=2
 # python train.py $root_dir --model vit_base_patch32_224 --pretrained --num-classes $num_class --output $output_dir -b 64 --dataset Adenoma --val-split test
 # python train.py $root_dir --model twins_pcpvt_base --pretrained --num-classes $num_class --output $output_dir -b 64 --dataset Adenoma --val-split test
 python train.py $root_dir --model swin_base_patch4_window7_224 --pretrained --num-classes $num_class --output $output_dir -b 64 --dataset Adenoma --val-split test
+python train.py $root_dir --model swin_base_patch4_window7_224 --pretrained --num-classes $num_class --output $output_dir -b 64 --dataset Adenoma --val-split test --use_balanced_sampler
 # python train.py $root_dir --model convit_base --pretrained --num-classes $num_class --output $output_dir -b 64 --dataset Adenoma --val-split test
 
 
