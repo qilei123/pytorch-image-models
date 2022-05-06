11a51159c00997c2f23830b5c754fd7f8ea03b2b
qilei123 <qileimail123@gmail.com>
2022-05-06 18:16:23 -0400

    test

--- train_adenoma.sh
+++ train_adenoma.sh
@@ -1,14 +1,16 @@
-root_dir=/data3/qilei_chen/DATA/polyp_xinzi/orig_4_classification
+#root_dir=/data3/qilei_chen/DATA/polyp_xinzi/orig_4_classification
+root_dir=/data3/qilei_chen/DATA/polyp_xinzi/D2/preprocessed
 output_dir=$root_dir/work_dir
 num_class=2
 # python train.py $root_dir --model mobilenetv3_large_100 --pretrained --num-classes $num_class --output $output_dir -b 64 --dataset Adenoma --val-split test
 # python train.py $root_dir --model densenet121 --pretrained --num-classes $num_class --output $output_dir -b 64 --dataset Adenoma --val-split test
-# python train.py $root_dir --model resnet50 --pretrained --num-classes $num_class --output $output_dir -b 64 --dataset Adenoma --val-split test
+# python train.py $root_dir --model resnext50_32x4d --pretrained --num-classes $num_class --output $output_dir -b 64 --dataset Adenoma --val-split test
 # python train.py $root_dir --model efficientnet_b0 --pretrained --num-classes $num_class --output $output_dir -b 64 --dataset Adenoma --val-split test
+
 #transformer
 # python train.py $root_dir --model vit_base_patch32_224 --pretrained --num-classes $num_class --output $output_dir -b 64 --dataset Adenoma --val-split test
 # python train.py $root_dir --model twins_pcpvt_base --pretrained --num-classes $num_class --output $output_dir -b 64 --dataset Adenoma --val-split test
-# python train.py $root_dir --model swin_base_patch4_window7_224 --pretrained --num-classes $num_class --output $output_dir -b 64 --dataset Adenoma --val-split test
+python train.py $root_dir --model swin_base_patch4_window7_224 --pretrained --num-classes $num_class --output $output_dir -b 64 --dataset Adenoma --val-split test
 # python train.py $root_dir --model convit_base --pretrained --num-classes $num_class --output $output_dir -b 64 --dataset Adenoma --val-split test
 
-python train.py $root_dir --model resnext50_32x4d --pretrained --num-classes $num_class --output $output_dir -b 64 --dataset Adenoma --val-split test
+
