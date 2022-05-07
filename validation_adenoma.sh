acab7f8889527d1aecc0915907fb4a8cb50b6632
qilei123 <qileimail123@gmail.com>
2022-05-07 00:37:37 -0400

    test

--- validation_adenoma.sh
+++ validation_adenoma.sh
@@ -1,6 +1,6 @@
-export data_dir=/data3/qilei_chen/DATA/polyp_xinzi/D2/
+export data_dir=/data3/qilei_chen/DATA/polyp_xinzi/D2/preprocessed/
 export num_classes=2
-export output_dir=/data3/qilei_chen/DATA/polyp_xinzi/preprocessed_4_classification/work_dir
+export output_dir=/data3/qilei_chen/DATA/polyp_xinzi/D2/preprocessed/work_dir
 export batch_size=64
 
 model_name=swin_base_patch4_window7_224
@@ -11,5 +11,5 @@ python validate.py $data_dir \
     --checkpoint $checkpoint_dir \
     -b $batch_size \
     --confusion-matrix \
-    --confusion-matrix-fig-dir $data_dir$model_name-224 \
+    --confusion-matrix-fig-dir $output_dir$model_name-224 \
     --dataset Adenoma --split test
